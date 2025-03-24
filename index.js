8c4a1e3b30bb16124720db02d3b1cb1ddd0fd35d
MUHAMMAD-MUNSIF <m.munsif4375@gmail.com>
2025-03-24 23:29:01 +0500

    I have created small worked.

---
+++ index.js
@@ -0,0 +1,19 @@
+const clickMe = () =>{
+    const title = document.getElementById("title")
+    title.style.fontSize = '60px'
+    title.style.color = 'red'
+
+    const decs = document.getElementById("decs")
+    decs.style.fontFamily = "Arial";
+    decs.style.backgroundColor = 'black'
+    decs.style.color = '#fff'
+    decs.style.padding = '10px'
+    decs.style.margin = '10px'
+
+    const btn = document.getElementById("btn")
+    btn.style.backgroundColor = 'green'
+    btn.style.color = '#fff'
+    btn.style.padding = '10px'
+    btn.style.margin = '10px'
+    btn.style.fontWeight = 'bold'
+}
